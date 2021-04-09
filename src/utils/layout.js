ef47e68984e4d932bc2f34cdc333c2b8fde5d1b1
Zach <praxis@target.com>
2021-04-09 09:33:22 -0500

    import navigation

--- src/utils/layout.js
+++ src/utils/layout.js
@@ -2,13 +2,13 @@ import React from 'react'
 import GlobalStyles from '@utils/GlobalStyles'
 import ThemeProvider from '@utils/Theme'
 import Div from '@atoms/Div'
-import Footer from '@organisms/footer'
+import Navigation from '@organisms/Navigation'
 
 const Layout = ({ children, ...rest }) => (
   <div {...rest}>
     <ThemeProvider>
       <Div />
-      <Footer />
+      <Navigation />
       <GlobalStyles />
       {children}
     </ThemeProvider>
