e64df1f2e8cfc05e5b1be5bf9ac6d7778c3c3e41
Zach <praxis@target.com>
2021-04-09 07:03:49 -0500

    testing removing failed import

--- src/utils/layout.js
+++ src/utils/layout.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import GlobalStyles from '@utils/GlobalStyles'
 import ThemeProvider from '@utils/Theme'
-import Navigation from '@organisms/Navigation'
 
 const Layout = ({ children, ...rest }) => (
   <div {...rest}>
     <ThemeProvider>
       <GlobalStyles />
-      <Navigation />
       {children}
     </ThemeProvider>
   </div>
