668a6eea25e975d1425e5402165e26100f831542
Zach <praxis@target.com>
2021-04-09 06:49:34 -0500

    dependency upgrade, webpack polyfill

---
+++ gatsby-node.js
@@ -0,0 +1,15 @@
+exports.onCreateWebpackConfig = ({
+  stage,
+  rules,
+  loaders,
+  plugins,
+  actions,
+}) => {
+  actions.setWebpackConfig({
+    resolve: {
+      fallback: {
+        path: require.resolve('path-browserify')
+      },
+    },
+  })
+}
